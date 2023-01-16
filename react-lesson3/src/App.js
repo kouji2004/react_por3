0a6217719442d24721505e2fca5c8cf27a65c797
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-16 10:49:18 +0900

    routingを設定してhomePageとBlogPageを行き来できる

--- react-lesson3/src/App.js
+++ react-lesson3/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import { BlogPage } from './components/BlogPage';
 import { Header } from './components/Header';
+import { HomePage } from './components/HomePage';
+import{ BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-
-      <Header/>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="blog" element={<BlogPage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- react-lesson3/src/components/Header.js
+++ react-lesson3/src/components/Header.js
@@ -2,6 +2,24 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <header className='logo'>
+        <h3>ポートフォリオ</h3>
+        <nav>
+          <ul>
+            <li>
+              <a href='#'>ホーム</a>
+            </li>
+            <li>
+              <a href='#'>ブログ</a>
+            </li>
+            <li>
+              <a href='#'>SNS</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+    </div>
   )
 }
